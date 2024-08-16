065aa642548a657ef4cd53a588368901041ac11e
Sul Kamara <sulaimankamara110@gmail.com>
2024-08-16 14:41:13 +0100

    Modulus Operator - Exercise 1 (Divisibility Check) - Completed

--- exercises.js
+++ exercises.js
@@ -1,2 +1,11 @@
 // Exercise 1
-function
+function isDivisible(n, m) {
+  if (n % m === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isDivisible(25, 5));
+console.log(isDivisible(16, 7));
