75abb3c79ff7832364419ad57607ffc8cbad9ac8
Sul Kamara <sulaimankamara110@gmail.com>
2024-08-16 15:14:55 +0100

    Modulus Operator - Exercise 3 (Repeating Number Sequence) - Completed (I think)?

--- exercises.js
+++ exercises.js
@@ -1,10 +1,12 @@
-// Exercise 2
-function parity(n) {
-  if (n % 2 === 0) {
-    return "even";
-  } else {
-    return "odd";
+// Exercise 3
+function pos(n) {
+  if (n % 3 === 0) {
+    return 15;
+  } else if (n % 3 === 1) {
+    return 5;
+  } else if (n % 3 === 2) {
+    return 10;
   }
 }
 
-console.log(parity(2), parity(3));
+console.log(pos(13412), pos(323), pos(43));
